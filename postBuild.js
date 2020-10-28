2e2c9e3ec42be627afa4a45174d3d4a054e98db8
Damien Gresham <loftylows@gmail.com>
2020-10-28 20:26:26 +0700

    update postBuild script

--- postBuild.js
+++ postBuild.js
@@ -1,19 +1,29 @@
 const fs = require("fs")
 const path = require("path")
-const fetch = require("node-fetch")
+const request = require("sync-request")
 
-const Headers = fetch.Headers
+const postBuild = () => {
+  console.log("sending static pages to cloudflare cache...")
 
-const postBuild = async () => {
   const fileItems = {}
+
+  /*
+  const url = process.env.CLOUDFLARE_BUILD_WORKER_URL
+  const CUSTOM_AUTH_KEY = process.env.CUSTOM_CLOUDFLARE_WORKER_AUTH_KEY
+  const CUSTOM_AUTH_KEY_HEADER = process.env.CUSTOM_AUTH_KEY_HEADER
+  */
 
   const url = process.env.CLOUDFLARE_BUILD_WORKER_URL
   const CUSTOM_AUTH_KEY = process.env.CUSTOM_CLOUDFLARE_WORKER_AUTH_KEY
   const CUSTOM_AUTH_KEY_HEADER = process.env.CUSTOM_AUTH_KEY_HEADER
 
-  if (!CUSTOM_AUTH_KEY || !CUSTOM_AUTH_KEY_HEADER || !url) return
+  if (!url || !CUSTOM_AUTH_KEY_HEADER || !CUSTOM_AUTH_KEY) return
 
   const pagesFolderPath = path.resolve(__dirname, ".next/server/pages")
+  if (!fs.existsSync(pagesFolderPath)) {
+    throw new Error("Directory not found at pagesFolderPath")
+  }
+
   const files = fs.readdirSync(pagesFolderPath, { withFileTypes: true })
   const staticPageFiles = files.filter((item) => item.isFile && path.extname(item.name) === ".html")
 
@@ -23,18 +33,15 @@ const postBuild = async () => {
     fileItems[item.name] = fileContent.toString()
   })
 
-  try {
-    await fetch(url, {
-      method: "POST",
-      body: JSON.stringify(fileItems),
-      headers: new Headers([
-        ["content-type", "application/json"],
-        [CUSTOM_AUTH_KEY_HEADER, CUSTOM_AUTH_KEY],
-      ]),
-    })
-  } catch (error) {
-    throw error
-  }
+  request("Post", url, {
+    headers: {
+      "content-type": "application/json",
+      [CUSTOM_AUTH_KEY_HEADER]: CUSTOM_AUTH_KEY,
+    },
+    body: JSON.stringify(fileItems),
+  })
+
+  console.log("finished sending static pages to cloudflare cache...")
 }
 
 postBuild()
